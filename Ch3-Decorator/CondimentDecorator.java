d4fd29e82a48faecc5b8a700ea76fb78980ced95
ComeDownToUs <blues4ceausescu@gmail.com>
2017-09-15 21:34:27 +0100

    Decorator Pattern

---
+++ Ch3-Decorator/CondimentDecorator.java
@@ -0,0 +1,4 @@
+public abstract class CondimentDecorator extends Beverage {
+    //needs to be interchangeable with beverage, hence extension
+    public abstract String getDescription();
+}
---
+++ Ch3-Decorator/README.md
@@ -0,0 +1,3 @@
+Decorator pattern represented through coffee orders built on a basis of:
+1. Core beverage (e.g. Dark Roast or Espresso)
+2-n. Additional contents (e.g. Mocha, whipped cream)
